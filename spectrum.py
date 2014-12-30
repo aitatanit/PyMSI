61ab19aaa4ab2b0d95ce358ff52069ad4ebc2554
mridulafmach <mridula.prasad@fmach.it>
2014-12-30 16:52:38 +0100

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -7,9 +7,10 @@ import numpy as np
 
 ## Small scale classes which identifies one spectrum
 class spectrum:
-    def __init__(self, mz, intensity):
+    def __init__(self, m):
         ''' Define the spectrum class '''
-        self.mz = mz
-        self.intensity = intensity
+        pippo = np.array(m)
+        self.mz = pippo[:,0]
+        self.intensity = pippo[:,1] 
         self.x = 0 
         self.y = 0
