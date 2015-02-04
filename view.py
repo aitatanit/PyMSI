23da5470c7c0d64bc4255cdff4cab0e2149e60b5
mridulafmach <mridula.prasad@fmach.it>
2015-02-04 11:47:52 +0100

    MSI 3d cube visualization

---
+++ view.py
@@ -0,0 +1,7 @@
+'''3-D msi cube visualization
+ipython should be start as 'ipython --pylab=wx'
+input required 3-D cube obtained from msiCube class'''
+
+ from spectral import *
+ 
+ view_cube(img, bands=[29, 19, 9])
