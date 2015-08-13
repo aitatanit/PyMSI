0fcc6827e89ed0d95c8835888c6390a511bf804a
mridulafmach <mridula.prasad@fmach.it>
2015-08-13 14:50:29 +0200

    Read data file from Analyze folder

---
+++ AnalyzeData.py
@@ -0,0 +1,40 @@
+''' Function PlotSpectra : to plot complete mass spectra for given index,
+    Function AnalyzeData : Take Folder path as input which contains several sub-folder as MSI data''' 
+
+
+from PyMSI.readMSI import readAnalyze
+from PyMSI.msiMatrix import msiMatrix
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import struct
+
+def PlotSpectra(mass,filename,id= '10',show='N'):
+     spectra = np.zeros(shape=(mass.size))
+     f = open(filename,'rb')
+     f.seek(mass.size * int(id) * 2)
+     for i in xrange(mass.size):
+       spectra[i] = struct.unpack('H',f.read(2))[0]
+     plt.plot(mass,spectra)
+     if(show == 'Y'):
+        plt.savefig('Spectraplot.jpg')
+     plt.show()
+     return(spectra)
+
+def AnalyzeData(path,massrange=[],matrix_save ='N',image_plot='N'):
+    listing = os.listdir(path)
+    for im in range(0,len(listing)):
+        address = path+listing[im]
+        print("Reading filename "+ str(listing[im]))
+        spec = readAnalyze(address,massrange)
+        mat = msiMatrix(spec)
+        Image = mat.matrix 
+        Image = np.sqrt(Image)
+        Image = np.ceil(Image)
+        if(matrix_save == 'Y'):
+           outputf = path+listing[im]+'.csv'
+           np.savetxt(outputf,Image, fmt='%-7.5f',delimiter=",")
+        if(image_plot == 'Y'):
+           outputp = path+listing[im]+'.jpg'
+           plt.imshow(Image,interpolation='None')
+           plt.savefig(outputp,bbox_inches='tight')
